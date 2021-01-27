09e14117bcdff33ebfd5786cc34fa470f068edac
Ivan Petrov <vanespetroff@gmail.com>
2021-01-27 22:11:31 +0300

    add memento + mediator

---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            LightBulb lightBulb = new LightBulb(mediator);
+            Switcher switcher = new Switcher(mediator);
+            
+            mediator.SetLightBulb(lightBulb);
+            mediator.SetSwitcher(switcher);
+
+            switcher.On();
+
+            switcher.Off();
+
+            lightBulb.BurnedOut();
+        }
+
+        class Mediator
+        {
+            private Switcher switcher;
+            private LightBulb lightBulb;
+
+            public void SetSwitcher(Switcher switcher)
+            {
+                this.switcher = switcher;
+            }
+
+            public void SetLightBulb(LightBulb lightBulb)
+            {
+                this.lightBulb = lightBulb;
+            }
+
+            public void Notify(Command command)
+            {
+                switch (command)
+                {
+                    case Command.TurnOnLigth:
+                        lightBulb.TurnOn();
+                        break;
+                    case Command.TurnOffLight:
+                        lightBulb.TurnOff();
+                        break;
+                    case Command.LightBulbBurnedOut:
+                        switcher.Off();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        enum Command
+        {
+            TurnOnLigth,
+            TurnOffLight,
+            LightBulbBurnedOut
+        }
+
+        class Switcher
+        {
+            private Mediator mediator;
+
+            public Switcher(Mediator mediator)
+            {
+                this.mediator = mediator;
+            }
+
+            public void On()
+            {
+                Console.WriteLine("Switcher activated");
+                mediator.Notify(Command.TurnOnLigth);
+            }
+
+            public void Off()
+            {
+                Console.WriteLine("Switcher deactivated");
+                mediator.Notify(Command.TurnOffLight);
+            }
+        }
+
+        class LightBulb
+        {
+            private Mediator mediator;
+            public LightBulb(Mediator mediator)
+            {
+                this.mediator = mediator;
+            }
+
+            public void TurnOn()
+            {
+                Console.WriteLine("Light bulb is on");
+            }
+
+            public void TurnOff()
+            {
+                Console.WriteLine("Light bulb is off");
+            }
+
+            public void BurnedOut()
+            {
+                Console.WriteLine("Light bulb burned out");
+                mediator.Notify(Command.LightBulbBurnedOut);
+            }
+        }
+    }
+}
---
+++ Memento/Memento/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memento
+{
+    class Program
+    {
+        //compare methods from mt example vs RefactoringGuru example
+        //memento what problem solves
+        static void Main(string[] args)
+        {
+            TextEditor textEditor = new TextEditor(new TextWindow());
+            textEditor.Write("number 1\n");
+            textEditor.Save();
+
+            textEditor.Write("number 2\n");
+            textEditor.Save();
+
+            textEditor.Write("number 3\n");
+            Console.WriteLine(textEditor.Print());
+
+            textEditor.Undo();
+
+            Console.WriteLine(textEditor.Print());
+
+            textEditor.Undo();
+
+            Console.WriteLine(textEditor.Print());
+        }
+
+        class TextEditor
+        {
+            private Stack<TextWindowMemento> savedTextWindows;
+            TextWindow textWindow;
+
+            public TextEditor(TextWindow textWindow)
+            {
+                savedTextWindows = new Stack<TextWindowMemento>();
+                this.textWindow = textWindow;
+            }
+
+            public string Print()
+            {
+                return textWindow.currentText;
+            }
+
+            public void Save()
+            {
+                savedTextWindows.Push(textWindow.Save());
+            }
+
+            public void Undo()
+            {
+                textWindow.Restore(savedTextWindows.Pop());
+            }
+
+            public void Write(string text)
+            {
+                textWindow.addText(text);
+            }
+        }
+
+        class TextWindowMemento
+        {
+            public string Text { get; private set; }
+
+            public TextWindowMemento(string text)
+            {
+                Text = text;
+            }
+        }
+
+        class TextWindow
+        {
+            public string currentText;
+
+            public void addText(string text)
+            {
+                currentText += text;
+            }
+
+            public TextWindowMemento Save()
+            {
+                return new TextWindowMemento(currentText);
+            }
+
+            public void Restore(TextWindowMemento memento)
+            {
+                currentText = memento.Text;
+            }
+        }
+    }
+}
